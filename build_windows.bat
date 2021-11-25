6d0b238017ccaa2da34f47a232207b9bc2b6fe67
Jakob Schwab <j.schwab@mail.de>
2021-11-25 21:16:41 +0100

    Better "buildsystem"

--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,27 @@
+@echo off
+
 pip3 install pyinstaller
 pip3 install cairosvg
-pyinstaller --add-binary "C:\Users\kerskuchen\AppData\Local\Programs\Python\Python39\Lib\site-packages\cairosvg";cairosvg --add-binary "C:\Users\kerskuchen\AppData\Local\Programs\Python\Python39\Lib\site-packages\cairocffi";cairocffi --specpath  build --name ToniToniChoppi --noconsole --onefile main.py
 
+if exist shipping_windows rmdir /s /q shipping_windows
+mkdir shipping_windows
+
+REM NOTE: robocopy has success error code 1
+robocopy "resources" "shipping_windows" /s /e > nul
+if %errorlevel% neq 1 goto :error
+
+pyinstaller --add-binary "C:\Users\kerskuchen\AppData\Local\Programs\Python\Python39\Lib\site-packages\cairosvg";cairosvg --add-binary "C:\Users\kerskuchen\AppData\Local\Programs\Python\Python39\Lib\site-packages\cairocffi";cairocffi --specpath  build --name ToniToniChoppi --noconsole --distpath "shipping_windows\ToniToniChoppi" --onefile main.py
+
+if %errorlevel% neq 0 goto :error
+goto :done
+
+REM ------------------------------------------------------------------------------------------------
+:error
+echo Failed with error #%errorlevel%.
 pause
+exit /b %errorlevel%
+
+REM ------------------------------------------------------------------------------------------------
+:done
+rmdir /s /q "temp"
+echo FINISHED BUILDING WINDOWS SHIPPING
---
+++ resources/ToniToniChoppi.bat
@@ -0,0 +1,2 @@
+echo off
+ToniToniChoppi\ToniToniChoppi.exe
